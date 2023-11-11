773950a463a2ed3e3d18ad7ec24ddee1a96515a6
bookofproofs <webmaster@bookofproofs.org>
2023-11-11 15:52:26 +0100

    bugfixes conjectures

---
+++ Fpl/FplLSTests/TestGetCompletionItemTheoremLikeStmts.cs
@@ -0,0 +1,143 @@
+using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+
+namespace FplLSTests
+{
+    [TestClass]
+    public class TestGetCompletionItemTheoremLikeStmts
+    {
+
+        [DataRow("inf", "Inference")]
+        [DataRow("inference", "Inference")]
+        [DataRow("thm", "Theorem")]
+        [DataRow("theorem", "Theorem")]
+        [DataRow("lem", "Lemma")]
+        [DataRow("lemma", "Lemma")]
+        [DataRow("conj", "Conjecture")]
+        [DataRow("conjecture", "Conjecture")]
+        [DataRow("prop", "Proposition")]
+        [DataRow("proposition", "Proposition")]
+        [TestMethod]
+        public void TestAddChoicesNumber(string choice, string l)
+        {
+            var detailCi = FplAutoCompleteService.GetDetail(choice);
+            var actual = FplAutoCompleteService.AddTheoremLikeStatementChoices(choice, l, detailCi);
+            Assert.AreEqual(2, actual.Count);
+        }
+
+        [DataRow("inf", "Inference")]
+        [DataRow("inference", "Inference")]
+        [DataRow("thm", "Theorem")]
+        [DataRow("theorem", "Theorem")]
+        [DataRow("lem", "Lemma")]
+        [DataRow("lemma", "Lemma")]
+        [DataRow("conj", "Conjecture")]
+        [DataRow("conjecture", "Conjecture")]
+        [DataRow("prop", "Proposition")]
+        [DataRow("proposition", "Proposition")]
+        [TestMethod]
+        public void TestAddKeywordCounts(string choice, string l)
+        {
+            var detailCi = FplAutoCompleteService.GetDetail(choice);
+            var actual = FplAutoCompleteService.AddTheoremLikeStatementChoices(choice, l, detailCi);
+            var count = 0;
+            foreach (var item in actual)
+            {
+                if (item.Kind == CompletionItemKind.Keyword) count++;
+            }
+            Assert.AreEqual(1, count);
+        }
+
+        [DataRow("inf", "Inference")]
+        [DataRow("inference", "Inference")]
+        [DataRow("thm", "Theorem")]
+        [DataRow("theorem", "Theorem")]
+        [DataRow("lem", "Lemma")]
+        [DataRow("lemma", "Lemma")]
+        [DataRow("conj", "Conjecture")]
+        [DataRow("conjecture", "Conjecture")]
+        [DataRow("prop", "Proposition")]
+        [DataRow("proposition", "Proposition")]
+        [TestMethod]
+        public void TestAddChoicesSortText(string choice, string l)
+        {
+            var detailCi = FplAutoCompleteService.GetDetail(choice);
+            var actual = FplAutoCompleteService.AddTheoremLikeStatementChoices(choice, l, detailCi);
+            foreach (var item in actual)
+            {
+                Assert.IsTrue(item.SortText.Contains(l.ToLower()));
+            }
+        }
+
+        [DataRow("inf", "Inference")]
+        [DataRow("inference", "Inference")]
+        [DataRow("thm", "Theorem")]
+        [DataRow("theorem", "Theorem")]
+        [DataRow("lem", "Lemma")]
+        [DataRow("lemma", "Lemma")]
+        [DataRow("conj", "Conjecture")]
+        [DataRow("conjecture", "Conjecture")]
+        [DataRow("prop", "Proposition")]
+        [DataRow("proposition", "Proposition")]
+        [TestMethod]
+        public void TestAddChoicesLabel(string choice, string l)
+        {
+            var detailCi = FplAutoCompleteService.GetDetail(choice);
+            var actual = FplAutoCompleteService.AddTheoremLikeStatementChoices(choice, l, detailCi);
+            foreach (var item in actual)
+            {
+                Assert.IsTrue(item.Label.Contains(choice) && item.Label.StartsWith("_ "));
+            }
+        }
+
+        [DataRow("inf", "Inference")]
+        [DataRow("inference", "Inference")]
+        [DataRow("thm", "Theorem")]
+        [DataRow("theorem", "Theorem")]
+        [DataRow("lem", "Lemma")]
+        [DataRow("lemma", "Lemma")]
+        [DataRow("conj", "Conjecture")]
+        [DataRow("conjecture", "Conjecture")]
+        [DataRow("prop", "Proposition")]
+        [DataRow("proposition", "Proposition")]
+        [TestMethod]
+        public void TestAddChoicesDetail(string choice, string l)
+        {
+            var detailCi = FplAutoCompleteService.GetDetail(choice);
+            var actual = FplAutoCompleteService.AddTheoremLikeStatementChoices(choice, l, detailCi);
+            foreach (var item in actual)
+            {
+                if (item.Kind == CompletionItemKind.Keyword)
+                {
+                    Assert.IsTrue(item.Detail.Contains(choice));
+                }
+                else
+                {
+                    Assert.IsTrue(item.Detail.Contains(l.ToLower()));
+                }
+            }
+        }
+
+        [DataRow("inf", "Inference")]
+        [DataRow("inference", "Inference")]
+        [DataRow("thm", "Theorem")]
+        [DataRow("theorem", "Theorem")]
+        [DataRow("lem", "Lemma")]
+        [DataRow("lemma", "Lemma")]
+        [DataRow("conj", "Conjecture")]
+        [DataRow("conjecture", "Conjecture")]
+        [DataRow("prop", "Proposition")]
+        [DataRow("proposition", "Proposition")]
+        [TestMethod]
+        public void TestAddChoicesInsertText(string choice, string l)
+        {
+            var detailCi = FplAutoCompleteService.GetDetail(choice);
+            var actual = FplAutoCompleteService.AddTheoremLikeStatementChoices(choice, l, detailCi);
+            var counterSnippets = 0;
+            foreach (var item in actual)
+            {
+                if (item.InsertText.Contains(choice)) { counterSnippets++; }
+            }
+            Assert.AreEqual(actual.Count, counterSnippets);
+        }
+    }
+}
