613577582910893f55e5325e2420e1462559cdff
bookofproofs <webmaster@bookofproofs.org>
2023-11-13 18:10:07 +0100

    bugfix definitions

--- Fpl/FplLSTests/TestGetCompletionItemDefinitions.cs
+++ Fpl/FplLSTests/TestGetCompletionItemDefinitions.cs
@@ -33,20 +33,49 @@ namespace FplLSTests
             Assert.AreEqual(3, count);
         }
 
-        [DataRow("def", "class")]
-        [DataRow("definition", "class")]
-        [DataRow("def", "predicate")]
-        [DataRow("definition", "predicate")]
-        [DataRow("def", "function")]
-        [DataRow("definition", "function")]
+        [DataRow("definition", "class", CompletionItemKind.Class, "definition01")]
+        [DataRow("definition", "predicate", CompletionItemKind.Class, "definition02")]
+        [DataRow("definition", "function", CompletionItemKind.Class, "definition03")]
+        [DataRow("def", "cl", CompletionItemKind.Class, "zdefinition01")]
+        [DataRow("def", "pred", CompletionItemKind.Class, "zdefinition02")]
+        [DataRow("def", "func", CompletionItemKind.Class, "zdefinition03")]
+        [DataRow("definition", "class", CompletionItemKind.Keyword, "zzzdefinition01")]
+        [DataRow("definition", "predicate", CompletionItemKind.Keyword, "zzzdefinition02")]
+        [DataRow("definition", "function", CompletionItemKind.Keyword, "zzzdefinition03")]
+        [DataRow("def", "cl", CompletionItemKind.Keyword, "zzzzdefinition01")]
+        [DataRow("def", "pred", CompletionItemKind.Keyword, "zzzzdefinition02")]
+        [DataRow("def", "func", CompletionItemKind.Keyword, "zzzzdefinition03")]
         [TestMethod]
-        public void TestAddDefinitionChoicesSortText(string choice, string subType)
+        public void TestAddPropertyChoicesSortText(string choice, string subType, CompletionItemKind isKeyword, string expected)
         {
             var detailCi = new FplCompletionItem(choice);
             var actual = new FplCompletionItemChoicesDefinition().GetChoices(detailCi);
             foreach (var item in actual)
             {
-                Assert.IsTrue(item.SortText.Contains("definition"));
+                if (item.Label.Contains(choice) && item.Label.Contains(subType) && item.Kind == isKeyword)
+                {
+                    Assert.AreEqual(expected, item.SortText);
+                }
+            }
+        }
+
+        [DataRow("def", "cl")]
+        [DataRow("definition", "class")]
+        [DataRow("def", "pred")]
+        [DataRow("definition", "predicate")]
+        [DataRow("def", "func")]
+        [DataRow("definition", "function")]
+        [TestMethod]
+        public void TestInsertTextEndsWithTwoNewLines(string choice, string l)
+        {
+            var detailCi = new FplCompletionItem(choice);
+            var actual = new FplCompletionItemChoicesDefinition().GetChoices(detailCi);
+            foreach (var item in actual)
+            {
+                if (item.Kind != CompletionItemKind.Keyword && item.InsertText.Contains(choice) && item.InsertText.Contains(l))
+                {
+                    Assert.IsTrue(item.InsertText.EndsWith(Environment.NewLine + Environment.NewLine));
+                }
             }
         }
 
@@ -64,9 +93,14 @@ namespace FplLSTests
             var counterRelated = 0;
             foreach (var item in actual)
             {
+                string postfix = "";
+                if (item.Kind != CompletionItemKind.Keyword)
+                {
+                    postfix = " ...";
+                }
                 if (item.Label.Contains(subType))
                 {
-                    Assert.AreEqual("_ " + choice + " " + subType, item.Label);
+                    Assert.AreEqual("_ " + choice + " " + subType + postfix, item.Label);
                     counterRelated++;
                 }
             }
