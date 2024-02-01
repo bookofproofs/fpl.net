ed6a0ba5b77a77acb47657196dfdd7bc00a1c4c8
bookofproofs <a_piotrowski@web.de>
2024-02-01 08:45:17 +0100

    completion items form infix, prefix, postfix, and symbol keywords

---
+++ Fpl/FplLS/FplCompletionItemChoicesSymbol.cs
@@ -0,0 +1,18 @@
+﻿using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+
+namespace FplLS
+{
+    public class FplCompletionItemChoicesSymbolic: FplCompletionItemChoices
+    {
+        public override List<FplCompletionItem> GetChoices(FplCompletionItem defaultCi) 
+        {
+            var ret = new List<FplCompletionItem>();
+            // snippet
+            var ci = defaultCi.Clone();
+            ci.InsertText = $"{defaultCi.Label.Substring(2)} {"\"...\""}";
+            ret.Add(ci);
+            return ret;
+
+        }
+    }
+}
