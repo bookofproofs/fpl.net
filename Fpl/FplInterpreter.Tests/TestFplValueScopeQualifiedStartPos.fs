1870cf0ab00e3c436134a226090ce35e5645a41c
bookofproofs <a_piotrowski@web.de>
2024-05-11 09:51:59 +0200

    more unit tests predicate QualifiedStartPos

--- Fpl/FplInterpreter.Tests/TestFplValueScopeQualifiedStartPos.fs
+++ Fpl/FplInterpreter.Tests/TestFplValueScopeQualifiedStartPos.fs
@@ -458,3 +458,116 @@ type TestFplValueScopeQualifiedStartPos() =
             | "ywc" -> Assert.IsTrue(ywc.QualifiedStartPos.Contains("Ln: 2, Col: 57)"))
             | _ -> Assert.IsTrue(false)
         | None -> Assert.IsTrue(false)
+
+    [<DataRow("base1")>]
+    [<DataRow("base2")>]
+    [<DataRow("base3")>]
+    [<DataRow("base4")>]
+    [<DataRow("base5")>]
+    [<DataRow("base6")>]
+    [<DataRow("base7")>]
+    [<DataRow("base8")>]
+    [<DataRow("base9")>]
+    [<DataRow("base10")>]
+    [<DataRow("base11")>]
+    [<DataRow("base12")>]
+    [<DataRow("base13")>]
+    [<DataRow("base11a")>]
+    [<DataRow("base12a")>]
+    [<DataRow("base10b")>]
+    [<DataRow("base11b")>]
+    [<DataRow("base12b")>]
+    [<DataRow("base13b")>]
+    [<DataRow("base10c")>]
+    [<DataRow("base11c")>]
+    [<DataRow("base12c")>]
+    [<DataRow("base13c")>]
+    [<DataRow("base10d")>]
+    [<DataRow("base11d")>]
+    [<DataRow("base12d")>]
+    [<DataRow("base13d")>]
+    [<DataRow("base10e")>]
+    [<DataRow("base11e")>]
+    [<DataRow("base12e")>]
+    [<DataRow("base13e")>]
+    [<DataRow("base10f")>]
+    [<DataRow("base11f")>]
+    [<DataRow("base12f")>]
+    [<DataRow("base13f")>]
+    [<DataRow("base14")>]
+    [<DataRow("base15")>]
+    [<DataRow("base16")>]
+    [<DataRow("base17")>]
+    [<DataRow("base18")>]
+    [<DataRow("base19")>]
+    [<DataRow("base20")>]
+    [<DataRow("base21")>]
+    [<DataRow("base22")>]
+    [<DataRow("base23")>]
+    [<DataRow("base24")>]
+    [<DataRow("base25")>]
+    [<DataRow("base26")>]
+    [<TestMethod>]
+    member this.TestPredicate(var) =
+        let result = CommonFplValueTestCases.ScopePredicate()
+        match result with
+        | Some (theory, base1,base2,base3,base4,base5, base6, base7, 
+                                    base8, base9, base10, base11, base12, base13,
+                                    base11a, base12a, base10b, base11b, base12b, base13b,
+                                    base10c, base11c, base12c, base13c, base10d, base11d,
+                                    base12d, base10e, base11e, base12e, base13d, base13e,
+                                    base10f, base11f, base12f, base13f, base14, base15,
+                                    base16, base17, base18, base19, base20, base21, base22,
+                                    base23, base24, base25, base26) ->
+            match var with
+            | "base1" -> Assert.IsTrue(base1.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base2" -> Assert.IsTrue(base2.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base3" -> Assert.IsTrue(base3.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base4" -> Assert.IsTrue(base4.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base5" -> Assert.IsTrue(base5.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base6" -> Assert.IsTrue(base6.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base7" -> Assert.IsTrue(base7.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base8" -> Assert.IsTrue(base8.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base9" -> Assert.IsTrue(base9.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base10" -> Assert.IsTrue(base10.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base11" -> Assert.IsTrue(base11.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base12" -> Assert.IsTrue(base12.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base13" -> Assert.IsTrue(base13.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base11a" -> Assert.IsTrue(base11a.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base12a" -> Assert.IsTrue(base12a.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base10b" -> Assert.IsTrue(base10b.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base11b" -> Assert.IsTrue(base11b.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base12b" -> Assert.IsTrue(base12b.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base13b" -> Assert.IsTrue(base13b.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base10c" -> Assert.IsTrue(base10c.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base11c" -> Assert.IsTrue(base11c.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base12c" -> Assert.IsTrue(base12c.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base13c" -> Assert.IsTrue(base13c.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base10d" -> Assert.IsTrue(base10d.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base11d" -> Assert.IsTrue(base11d.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base12d" -> Assert.IsTrue(base12d.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base13d" -> Assert.IsTrue(base13d.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base10e" -> Assert.IsTrue(base10e.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base11e" -> Assert.IsTrue(base11e.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base12e" -> Assert.IsTrue(base12e.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base13e" -> Assert.IsTrue(base13e.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base10f" -> Assert.IsTrue(base10f.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base11f" -> Assert.IsTrue(base11f.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base12f" -> Assert.IsTrue(base12f.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base13f" -> Assert.IsTrue(base13f.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base14" -> Assert.IsTrue(base14.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base15" -> Assert.IsTrue(base15.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base16" -> Assert.IsTrue(base16.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base17" -> Assert.IsTrue(base17.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base18" -> Assert.IsTrue(base18.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base19" -> Assert.IsTrue(base19.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base20" -> Assert.IsTrue(base20.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base21" -> Assert.IsTrue(base21.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base22" -> Assert.IsTrue(base22.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base23" -> Assert.IsTrue(base23.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base24" -> Assert.IsTrue(base24.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base25" -> Assert.IsTrue(base25.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | "base26" -> Assert.IsTrue(base26.QualifiedStartPos.Contains("Ln: 1, Col: 1)"))
+            | _ -> Assert.IsTrue(false)
+        | None -> 
+            Assert.IsTrue(false)
