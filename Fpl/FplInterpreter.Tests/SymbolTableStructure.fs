5a8f16d6a96e988ea29ebc58485e3cb35d1e72ee
bookofproofs <eite@web.de>
2025-09-15 08:17:31 +0200

    refactoring symbolTable-related tests

--- Fpl/FplInterpreter.Tests/SymbolTableStructure.fs
+++ Fpl/FplInterpreter.Tests/SymbolTableStructure.fs
@@ -1,13 +1,240 @@
 namespace FplInterpreter.Tests
+open System
 open Microsoft.VisualStudio.TestTools.UnitTesting
 open FParsec
+open ErrDiagnostics
 open FplPrimitives
 open FplInterpreterTypes
+open CommonTestHelpers
 
 [<TestClass>]
-type TestFplBlockType() =
+type SymbolTableStructure() =
     let positions = (Position("",0,0,0), Position("",0,0,0))
     let parent = new FplRoot()
+
+    let getName nodeType = 
+        match nodeType with
+        | "FplArgInferenceAssume" ->
+            let x = new FplArgInferenceAssume(positions, parent)
+            x.Name
+        | "FplArgInferenceDerived" ->
+            let x = new FplArgInferenceDerived(positions, parent)
+            x.Name
+        | "FplArgInferenceRevoke" ->
+            let x = new FplArgInferenceRevoke(positions, parent)
+            x.Name
+        | "FplArgInferenceTrivial" ->
+            let x = new FplArgInferenceTrivial(positions, parent)
+            x.Name
+        | "FplArgument" ->
+            let x = new FplArgument(positions, parent, 0)
+            x.Name
+        | "FplAssertion" ->
+            let x = new FplAssertion(positions, parent)
+            x.Name
+        | "FplAssignment" ->
+            let x = new FplAssignment(positions, parent)
+            x.Name
+        | "FplAxiom" ->
+            let x = new FplAxiom(positions, parent, 0)
+            x.Name
+        | "FplCases" ->
+            let x = new FplCases(positions, parent)
+            x.Name
+        | "FplCaseElse" ->
+            let x = new FplCaseElse(positions, parent)
+            x.Name
+        | "FplCaseSingle" ->
+            let x = new FplCaseSingle(positions, parent)
+            x.Name
+        | "FplClass" ->
+            let x = new FplClass(positions, parent)
+            x.Name
+        | "FplConjecture" ->
+            let x = new FplConjecture(positions, parent, 0)
+            x.Name
+        | "FplConjunction" ->
+            let x = new FplConjunction(positions, parent)
+            x.Name
+        | "FplConstructor" ->
+            let x = new FplConstructor(positions, parent)
+            x.Name
+        | "FplCorollary" ->
+            let x = new FplCorollary(positions, parent, 0)
+            x.Name
+        | "FplDecrement" ->
+            let x = new FplDecrement(positions, parent)
+            x.Name
+        | "FplDisjunction" ->
+            let x = new FplDisjunction(positions, parent)
+            x.Name
+        | "FplEquality" ->
+            let x = new FplEquality(positions, parent)
+            x.Name
+        | "FplEquivalence" ->
+            let x = new FplEquivalence(positions, parent)
+            x.Name
+        | "FplExclusiveOr" ->
+            let x = new FplExclusiveOr(positions, parent)
+            x.Name
+        | "FplExtension" ->
+            let x = new FplExtension(positions, parent)
+            x.Name
+        | "FplExtensionObj" ->
+            let x = new FplExtensionObj(positions, parent)
+            x.Name
+        | "FplForInStmt" ->
+            let x = new FplForInStmt(positions, parent)
+            x.Name
+        | "FplForInStmtDomain" ->
+            let x = new FplForInStmtDomain(positions, parent)
+            x.Name
+        | "FplForInStmtEntity" ->
+            let x = new FplForInStmtEntity(positions, parent)
+            x.Name
+        | "FplFunctionalTerm" ->
+            let x = new FplFunctionalTerm(positions, parent, 0)
+            x.Name
+        | "FplImplication" ->
+            let x = new FplImplication(positions, parent)
+            x.Name
+        | "FplInstance" ->
+            let x = new FplInstance(positions, parent)
+            x.Name
+        | "FplIntrinsicFunc" ->
+            let x = new FplIntrinsicFunc(positions, parent)
+            x.Name
+        | "FplIntrinsicInd" ->
+            let x = new FplIntrinsicInd(positions, parent)
+            x.Name
+        | "FplIntrinsicObj" ->
+            let x = new FplIntrinsicObj(positions, parent)
+            x.Name
+        | "FplIntrinsicPred" ->
+            let x = new FplIntrinsicPred(positions, parent)
+            x.Name
+        | "FplIntrinsicTpl" ->
+            let x = new FplIntrinsicTpl(positions, parent)
+            x.Name
+        | "FplIntrinsicUndef" ->
+            let x = new FplIntrinsicUndef(positions, parent)
+            x.Name
+        | "FplIsOperator" ->
+            let x = new FplIsOperator(positions, parent)
+            x.Name
+        | "FplJustification" ->
+            let x = new FplJustification(positions, parent)
+            x.Name
+        | "FplJustificationItemByAx" ->
+            let x = new FplJustificationItemByAx(positions, parent, 0)
+            x.Name
+        | "FplJustificationItemByCor" ->
+            let x = new FplJustificationItemByCor(positions, parent, 0)
+            x.Name
+        | "FplJustificationItemByDef" ->
+            let x = new FplJustificationItemByDef(positions, parent, 0)
+            x.Name
+        | "FplJustificationItemByInf" ->
+            let x = new FplJustificationItemByInf(positions, parent, 0)
+            x.Name
+        | "FplJustificationItemByProofArgument" ->
+            let x = new FplJustificationItemByProofArgument(positions, parent, 0)
+            x.Name
+        | "FplJustificationItemByTheoremLikeStmt" ->
+            let x = new FplJustificationItemByTheoremLikeStmt(positions, parent, 0)
+            x.Name
+        | "FplLanguage" ->
+            let x = new FplLanguage(positions, parent)
+            x.Name
+        | "FplLemma" ->
+            let x = new FplLemma(positions, parent, 0)
+            x.Name
+        | "FplLocalization" ->
+            let x = new FplLocalization(positions, parent)
+            x.Name
+        | "FplMandatoryFunctionalTerm" ->
+            let x = new FplMandatoryFunctionalTerm(positions, parent)
+            x.Name
+        | "FplMandatoryPredicate" ->
+            let x = new FplMandatoryPredicate(positions, parent)
+            x.Name
+        | "FplMapCaseElse" ->
+            let x = new FplMapCaseElse(positions, parent)
+            x.Name
+        | "FplMapCaseSingle" ->
+            let x = new FplMapCaseSingle(positions, parent)
+            x.Name
+        | "FplMapCases" ->
+            let x = new FplMapCases(positions, parent)
+            x.Name
+        | "FplMapping" ->
+            let x = new FplMapping(positions, parent)
+            x.Name
+        | "FplNegation" ->
+            let x = new FplNegation(positions, parent)
+            x.Name
+        | "FplOptionalFunctionalTerm" ->
+            let x = new FplOptionalFunctionalTerm(positions, parent)
+            x.Name
+        | "FplOptionalPredicate" ->
+            let x = new FplOptionalPredicate(positions, parent)
+            x.Name
+        | "FplPredicate" ->
+            let x = new FplPredicate(positions, parent, 0)
+            x.Name
+        | "FplPremiseList" ->
+            let x = new FplPremiseList(positions, parent, 0)
+            x.Name
+        | "FplProof" ->
+            let x = new FplProof(positions, parent, 0)
+            x.Name
+        | "FplProposition" ->
+            let x = new FplProposition(positions, parent, 0)
+            x.Name
+        | "FplQuantorAll" ->
+            let x = new FplQuantorAll(positions, parent)
+            x.Name
+        | "FplQuantorExists" ->
+            let x = new FplQuantorExists(positions, parent)
+            x.Name
+        | "FplQuantorExistsN" ->
+            let x = new FplQuantorExistsN(positions, parent)
+            x.Name
+        | "FplReference" ->
+            let x = new FplReference(positions, parent)
+            x.Name
+        | "FplReturn" ->
+            let x = new FplReturn(positions, parent)
+            x.Name
+        | "FplRoot" ->
+            let x = new FplRoot()
+            x.Name
+        | "FplRuleOfInference" ->
+            let x = new FplRuleOfInference(positions, parent, 0)
+            x.Name
+        | "FplTheorem" ->
+            let x = new FplTheorem(positions, parent, 0)
+            x.Name
+        | "FplTheory" ->
+            let x = new FplTheory(positions, parent, "", 0)
+            x.Name
+        | "FplTranslation" ->
+            let x = new FplTranslation(positions, parent)
+            x.Name
+        | "FplVariable" -> 
+            let x = new FplVariable(positions, parent) 
+            x.Name
+        | "FplVariadicVariableMany" ->
+            let x = new FplVariable(positions, parent)
+            x.SetToMany() |> ignore
+            x.Name
+        | "FplVariadicVariableMany1" ->
+            let x = new FplVariable(positions, parent)
+            x.SetToMany1() |> ignore
+            x.Name
+        | _ -> 
+            failwith $"Unknown node type {nodeType}"
+
 
     [<DataRow("FplArgInferenceAssume")>]
     [<DataRow("FplArgInferenceDerived")>]
@@ -1498,3 +1725,121 @@ type TestFplBlockType() =
         with
         | :? System.NotImplementedException -> Assert.Fail("Run method not implmented")
         | _ -> ()
+
+
+    [<DataRow("FplArgInferenceAssume", "00", """proof T$1 {1. |- assume 1};""")>]
+    [<DataRow("FplArgInferenceDerived", "00", """;""")>]
+    [<DataRow("FplArgInferenceRevoke", "00", """;""")>]
+    [<DataRow("FplArgInferenceTrivial", "00", """;""")>]
+    [<DataRow("FplArgument", "00", """;""")>]
+    [<DataRow("FplAssertion", "00", """;""")>]
+    [<DataRow("FplAssignment", "00", """def pred T() {dec ~x:pred x:=false; true};""")>]
+    [<DataRow("FplAxiom", "00", """;""")>]
+    [<DataRow("FplCases", "00", """;""")>]
+    [<DataRow("FplCaseElse", "00", """;""")>]
+    [<DataRow("FplCaseSingle", "00", """;""")>]
+    [<DataRow("FplClass", "00", """;""")>]
+    [<DataRow("FplConjecture", "00", """;""")>]
+    [<DataRow("FplConjunction", "00", """;""")>]
+    [<DataRow("FplConstructor", "00", """;""")>]
+    [<DataRow("FplCorollary", "00", """;""")>]
+    [<DataRow("FplDecrement", "00", """;""")>]
+    [<DataRow("FplDisjunction", "00", """;""")>]
+    [<DataRow("FplEquality", "00", """;""")>]
+    [<DataRow("FplEquivalence", "00", """;""")>]
+    [<DataRow("FplExclusiveOr", "00", """;""")>]
+    [<DataRow("FplExtension", "00", """;""")>]
+    [<DataRow("FplExtensionObj", "00", """;""")>]
+    [<DataRow("FplForInStmt", "00", """;""")>]
+    [<DataRow("FplForInStmtDomain", "00", """;""")>]
+    [<DataRow("FplForInStmtEntity", "00", """;""")>]
+    [<DataRow("FplFunctionalTerm", "00", """;""")>]
+    [<DataRow("FplImplication", "00", """;""")>]
+    [<DataRow("FplInstance", "00", """;""")>]
+    [<DataRow("FplIntrinsicFunc", "00", """;""")>]
+    [<DataRow("FplIntrinsicInd", "00", """;""")>]
+    [<DataRow("FplIntrinsicObj", "00", """;""")>]
+    [<DataRow("FplIntrinsicPred", "00", """;""")>]
+    [<DataRow("FplIntrinsicTpl", "00", """;""")>]
+    [<DataRow("FplIntrinsicUndef", "00", """;""")>]
+    [<DataRow("FplIsOperator", "00", """;""")>]
+    [<DataRow("FplJustification", "00", """;""")>]
+    [<DataRow("FplJustificationItemByAx", "00", """;""")>]
+    [<DataRow("FplJustificationItemByCor", "00", """;""")>]
+    [<DataRow("FplJustificationItemByDef", "00", """;""")>]
+    [<DataRow("FplJustificationItemByInf", "00", """inf ExistsByExample() {pre:true con:true} T$1 { 100. ExistsByExample, 1 |- false };""")>]
+    [<DataRow("FplJustificationItemByProofArgument", "00", """;""")>]
+    [<DataRow("FplJustificationItemByTheoremLikeStmt", "00", """;""")>]
+    [<DataRow("FplLanguage", "00", """;""")>]
+    [<DataRow("FplLemma", "00", """;""")>]
+    [<DataRow("FplLocalization", "00", """;""")>]
+    [<DataRow("FplMandatoryFunctionalTerm", "00", """;""")>]
+    [<DataRow("FplMandatoryPredicate", "00", """;""")>]
+    [<DataRow("FplMapCases", "00", """;""")>]
+    [<DataRow("FplMapCaseElse", "00", """;""")>]
+    [<DataRow("FplMapCaseSingle", "00", """;""")>]
+    [<DataRow("FplMapping", "00", """;""")>]
+    [<DataRow("FplNegation", "00", """;""")>]
+    [<DataRow("FplOptionalFunctionalTerm", "00", """;""")>]
+    [<DataRow("FplOptionalPredicate", "00", """;""")>]
+    [<DataRow("FplPredicate", "00", """;""")>]
+    [<DataRow("FplPremiseList", "00", """;""")>]
+    [<DataRow("FplProof", "00", """;""")>]
+    [<DataRow("FplProposition", "00", """;""")>]
+    [<DataRow("FplQuantorAll", "00", """;""")>]
+    [<DataRow("FplQuantorExists", "00", """;""")>]
+    [<DataRow("FplQuantorExistsN", "00", """;""")>]
+    [<DataRow("FplReference", "00", """;""")>]
+    [<DataRow("FplReturn", "00", """;""")>]
+    [<DataRow("FplRoot", "00", """;""")>]
+    [<DataRow("FplRuleOfInference", "00", """;""")>]
+    [<DataRow("FplTheorem", "00", """;""")>]
+    [<DataRow("FplTheory", "00", """;""")>]
+    [<DataRow("FplTranslation", "00", """;""")>]
+    [<DataRow("FplVariable", "00", """;""")>]
+    [<DataRow("FplVariable", "00", """;""")>]
+    [<DataRow("FplVariadicVariableMany", "00", """;""")>]
+    [<DataRow("FplVariadicVariableMany1", "00", """;""")>]
+    [<TestMethod>]
+    member this.TestStructure(nodeType, varVal, fplCode) =
+        let rec findNamedItem firstTypeNode (root:FplValue) = 
+            if root.Name = firstTypeNode then 
+                Some root
+            else
+                match root.Scope.Values |> Seq.tryPick (findNamedItem firstTypeNode) with 
+                | Some found -> Some found
+                | _ -> root.ArgList |> Seq.tryPick (findNamedItem firstTypeNode)
+        ad.Clear()
+        let filename = "TestStructure.fpl"
+        let stOption = prepareFplCode(filename + ".fpl", fplCode, false) 
+        prepareFplCode(filename, "", false) |> ignore
+        match stOption with
+        | Some st -> 
+            let nodeName = (getName nodeType)
+            let testNodeOpt = findNamedItem nodeName st.Root
+            match testNodeOpt with 
+            | Some (node:FplValue) ->
+                let parent = node.Parent.Value 
+                let argCount = node.ArgList.Count
+                let scopeCount = node.Scope.Count
+                match nodeType, varVal with
+                | "FplArgInferenceAssume", "00" -> 
+                    Assert.IsInstanceOfType<FplArgument>(parent)
+                    Assert.IsInstanceOfType<FplArgInferenceAssume>(node)
+                    Assert.AreEqual<int>(1, argCount)
+                    Assert.AreEqual<int>(0, scopeCount)
+                | "FplAssignment", "00" -> 
+                    Assert.IsInstanceOfType<FplPredicate>(parent)
+                    Assert.IsInstanceOfType<FplAssignment>(node)
+                    Assert.AreEqual<int>(2, argCount)
+                    Assert.AreEqual<int>(0, scopeCount)
+                | "FplTheory", "00" ->
+                    Assert.IsInstanceOfType<FplRoot>(parent)
+                    Assert.IsInstanceOfType<FplTheory>(node)
+                    Assert.AreEqual<int>(0, argCount)
+                    Assert.AreEqual<int>(0, scopeCount)
+                | _ -> failwith($"unmatched test {nodeType} {varVal}")
+            | None ->
+                failwith($"Nodetype {nodeType} not found in symbol table. Test is not implemented correctly.")
+        | None -> 
+            failwith($"FPL code could not be interpreted due to errors {Environment.NewLine}{ad.DiagnosticsToString}")
