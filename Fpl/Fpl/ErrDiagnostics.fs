ebc08e24b656383ed4458eebfaab0829cbe347d2
bookofproofs <eite@web.de>
2026-01-24 17:40:26 +0100

    test(SIG014): add more tests for cases stmt

--- Fpl/Fpl/ErrDiagnostics.fs
+++ Fpl/Fpl/ErrDiagnostics.fs
@@ -437,7 +437,7 @@ type DiagnosticCode =
             | SIG10 (arrName, indexVarName, indexNumber) -> $"Array `{arrName}` has less dimensions, {englishOrdinal indexNumber} index `{indexVarName}` not supported"
             | SIG11 (qualifiedNameMapping, qualifiedWrongCandidate) -> $"{qualifiedNameMapping} cannot map to {qualifiedWrongCandidate}"
             | SIG12 (templateName, secondUsage, firstUsage, firstUsagePos) -> $"The template `{templateName}` was used inconsistently with `{secondUsage}`, expecting `{firstUsage}` as it was used at `{firstUsagePos}`."
-            | SIG13 (stmtName, secondUsage, firstUsage, firstUsagePos) -> $"This case of the {stmtName} must return a value with a type of the first case at `{firstUsagePos}`, which was `{firstUsage}`. This case returns `{secondUsage}`."
+            | SIG13 (stmtName, secondUsage, firstUsage, firstUsagePos) -> $"Every branch of the {stmtName} must return a value with a type of the first case at `{firstUsagePos}`, which was `{firstUsage}`. This branch returns `{secondUsage}`."
             | SIG14 -> $"This case will never be matched."
             // structure-related error codes
             | ST001 nodeName -> sprintf $"The {nodeName} does nothing."
--- Fpl/FplInterpreter.Tests/TestInterpreterErrors.fs
+++ Fpl/FplInterpreter.Tests/TestInterpreterErrors.fs
@@ -3521,6 +3521,12 @@ type TestInterpreterErrors() =
     [<DataRow("03", """def pred T() { mcases (| (x + 1 = 2) : $1 | (x + 1 = 3) : $2 ? undef) };""", 0)>] 
     [<DataRow("04", """def pred T() { mcases (| (x + 1 = 2) : $1 | (x + 1 = 2) : $2 ? undef) };""", 1)>] // second condition will never be reached
     [<DataRow("05", """def pred T() { mcases (| ex x:obj { (x = 2) } : $1 | (x + 1 = 2) : $2 | ex x:obj { (x = 2) } : $3 | ex x:obj { (x = 2) } : $4 ? undef) };""", 2)>] // third and forth condition will never be reached
+    [<DataRow("06", """def pred T() { dec ~res:pred cases (| true : res:=true | false : res:=true ? res:=undef); res };""", 0)>] 
+    [<DataRow("07", """def pred T() { dec ~res:pred cases (| true : res:=$1 | true : res:=$2 ? res:=undef); res };""", 1)>] // second condition will never be reached
+    [<DataRow("08", """def pred T() { dec ~res:pred cases (| (x + 1 = 2) : res:=$1 | true : res:=$2 ? res:=undef); res };""", 0)>] 
+    [<DataRow("09", """def pred T() { dec ~res:pred cases (| (x + 1 = 2) : res:=$1 | (x + 1 = 3) : res:=$2 ? res:=undef); res };""", 0)>] 
+    [<DataRow("10", """def pred T() { dec ~res:pred cases (| (x + 1 = 2) : res:=$1 | (x + 1 = 2) : res:=$2 ? res:=undef); res };""", 1)>] // second condition will never be reached
+    [<DataRow("11", """def pred T() { dec ~res:pred cases (| ex x:obj { (x = 2) } : res:=$1 | (x + 1 = 2) : res:=$2 | ex x:obj { (x = 2) } : res:=$3 | ex x:obj { (x = 2) } : res:=$4 ? res:=undef); res };""", 2)>] // third and forth condition will never be reached
     [<DataRow("99", "uses Fpl.Commons.Structures ;", 0)>]
     [<TestMethod>]
     member this.TestSIG14(no:string, fplCode:string, expected) =
