a9ed1eb86deb38e6b698c5585ad330da8fa30bc4
bookofproofs <eite@web.de>
2026-09-19 15:50:18 +0200

    refactor(solution):migrate TextDocumentSyncHandler.cs to F#

--- Fpl/FplLS/Program.cs
+++ Fpl/FplLS/Program.cs
@@ -29,6 +29,7 @@ using OmniSharp.Extensions.LanguageServer.Server;
 using static Fpl.Errors.Diagnostics;
 using static Fpl.Interpreter.SymbolTable.Storage.Heap;
 using static FplLsLib.Buffers.BuffMgr;
+using static FplLsLib.Buffers.DocSync;
 using static FplLsLib.ServicesDiagnostics.Diags;
 
 namespace FplLS
