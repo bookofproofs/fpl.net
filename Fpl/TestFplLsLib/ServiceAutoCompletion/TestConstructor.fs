ad32787f68bf812f365f6910fa5797efba54301f
bookofproofs <eite@web.de>
2026-09-20 09:12:26 +0200

    refactor(tests):add f#-based auto complete test project

---
+++ Fpl/TestFplLsLib/ServiceAutoCompletion/TestConstructor.fs
@@ -0,0 +1,8 @@
+namespace TestFplLsLib.ServiceAutoCompletion
+
+open System
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open OmniSharp.Extensions.LanguageServer.Protocol.Models
+open FplLsLib.ServiceAutoCompletion.Constructor
+open Fpl.Primitives
+
